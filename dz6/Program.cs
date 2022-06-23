7c8adf97b22ebcb2b5960e43fa42e21d740e043e
Natalia <rng2010@mail.ru>
2022-06-24 01:50:53 +0400

    ДЗ к 6 семинару

--- dz6/Program.cs
+++ dz6/Program.cs
@@ -31,20 +31,40 @@ VvodNumber(M);
 Console.WriteLine($"Введено {Colichestvopositivenumber(array)} чисел больше нуля ");
 */
 
-int[]  CreateRandomArray(int size, int min, int max)
+
+
+
+int []  SpecifiedArray(int size)
 {
     int[] newArray = new int[size];
-
+    Console.WriteLine("Заданный массив");
     for(int i = 0; i < size; i++)
     {
-        newArray[i] = new Random().Next(min, max + 1); 
-        Console.Write(newArray[i] + " ");
+        newArray[i] = new Random().Next(0,100); 
+        Console.Write(newArray[i]+ " ");
+
     }
     Console.WriteLine();
     return newArray;
-
 }
 
+
+
+void CopyArray(int[] array1, int[] array2)
+{
+    Console.WriteLine("Копия массива");
+    for (int i = 0; i < array1.Length; i++)
+    {
+        array1[i] = array2[i]; 
+        Console.Write(array1[i]+ " ");
+    }
+   
+}
+    
+
+Console.WriteLine("Введите количество элементов в массиве ");
+int size1 = Convert.ToInt32(Console.ReadLine());
+CopyArray(new int[size1],SpecifiedArray(size1));
 
 
 
