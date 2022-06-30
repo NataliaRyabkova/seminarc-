b1a76bdc3c0f70891e70a4ed40d93b9b66137cf0
Natalia <rng2010@mail.ru>
2022-06-30 13:38:05 +0400

    двумерные массивы продолжение

--- seminar8/Program.cs
+++ seminar8/Program.cs
@@ -85,17 +85,22 @@ int[,] DelitStroka(int[,] array)
     int min = array[imin,jmin]; 
 
      for (int i = 0; i < array.GetLength(0); i ++)
+     {
             for (int j = 0; j < array.GetLength(1); j++)
             {
-                if(array[i,j] < min) min = array[i,j]; 
+                if(array[i,j] < min)
+                {
+                min = array[i,j]; 
                 imin = i;  
-                jmin = j;  
-            }          
+                jmin = j; 
+                } 
+            }  
+     }        
             
-           for (int i = 0; i < array.GetLength(0); i ++)
-           array[i,jmin] = 0;
-           for (int j = 0; j < array.GetLength(1); j++)
-           array [imin,j] = 0;
+for (int i = 0; i < array.GetLength(0); i ++)
+    array[i,jmin] = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [imin,j] = 0;
 return array;
 }
 
